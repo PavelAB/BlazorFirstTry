d8ee2af9cdd8c5c543bb35a101d8fc4ed5639d22
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-10-01 12:15:59 +0200

    feat(BlazorFirstTry): Game miner with mediator pattern v0.1

---
+++ BlazorFirstTry/Tools/Mediator.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace BlazorFirstTry.Tools
+{
+    public class Mediator<TMessage>
+    {
+        public static Mediator<TMessage> Instance = new();
+
+        private Mediator() { }
+
+
+        private Action<TMessage>? _broadcast;
+
+        public void Register(Action<TMessage> action)
+        {
+            _broadcast += action;
+        }
+        public void Unregister(Action<TMessage> action)
+        {
+            _broadcast -= action;
+        }
+        public void Send(TMessage message)
+        {
+            _broadcast?.Invoke(message);
+        }
+
+
+    }
+}
