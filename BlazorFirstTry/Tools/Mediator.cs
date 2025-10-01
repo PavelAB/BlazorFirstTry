d27151858aa6d3d6f0ad8dff3268d5fe3be52c4f
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-10-01 15:12:54 +0200

    feat(BlazorFirstTry): Game miner with mediator pattern v0.2

--- BlazorFirstTry/Tools/Mediator.cs
+++ BlazorFirstTry/Tools/Mediator.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel;
+﻿using BlazorFirstTry.Models;
+using System.ComponentModel;
 
 namespace BlazorFirstTry.Tools
 {
+    public record KeyForDict(int X, int Y);
     public class Mediator<TMessage>
     {
         public static Mediator<TMessage> Instance = new();
@@ -9,19 +11,31 @@ namespace BlazorFirstTry.Tools
         private Mediator() { }
 
 
-        private Action<TMessage>? _broadcast;
+        //private Action< TMessage>? _broadcast;
+        private Dictionary<KeyForDict, Action<TMessage>> _box = new();
 
-        public void Register(Action<TMessage> action)
+        public void Register(KeyForDict key, Action<TMessage> action)
         {
-            _broadcast += action;
+            if(!(_box.ContainsKey(key)))
+                _box.Add(key, action);
         }
-        public void Unregister(Action<TMessage> action)
+        public void Unregister(KeyForDict key, Action<TMessage> action)
         {
-            _broadcast -= action;
+            if (_box.ContainsKey(key))
+                _box.Remove(key);
         }
-        public void Send(TMessage message)
+        public void Send(KeyForDict key, TMessage message)
         {
-            _broadcast?.Invoke(message);
+            IEnumerable<Action<TMessage>> _broadcast = _box
+                .Where(c =>
+                    Math.Abs(key.X - c.Key.X) < 2 &&
+                    Math.Abs(key.Y - c.Key.Y) < 2 &&
+                    !(key.X == c.Key.X && key.Y == c.Key.Y)).Select(c => c.Value);
+
+            foreach(Action<TMessage> action in _broadcast)
+            {
+                action.Invoke(message);
+            }
         }
 
 
