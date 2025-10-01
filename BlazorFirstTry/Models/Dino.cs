4680ba8d068abce4dd57b7901b8b271ebae77340
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-10-01 16:34:13 +0200

    feat(BlazorFirstTry): Connect to DinoApi, and implement dinoList component

---
+++ BlazorFirstTry/Models/Dino.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorFirstTry.Models
+{
+    public class Dino
+    {
+        public Dino(int dinoID, string spece, double weight, double size)
+        {
+            DinoID = dinoID;
+            Spece = spece;
+            Weight = weight;
+            Size = size;
+        }
+
+        public int DinoID { get; set; }
+        public string Spece { get; set; }
+        public double Weight { get; set; }
+        public double Size { get; set; }
+    }
+}
--- BlazorFirstTry/Program.cs
+++ BlazorFirstTry/Program.cs
@@ -13,6 +13,12 @@ namespace BlazorFirstTry
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddHttpClient("DinoAPi", options =>
+            {
+                options.BaseAddress = new Uri("https://localhost:7199");
+            });
+
+
             await builder.Build().RunAsync();
         }
     }
